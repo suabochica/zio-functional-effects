1c3a8d0bf3b700d7cf7def2b734726784dce9c4e
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-09-14 16:07:54 -0500

    zionomicon: collectAll exercise using zipWith (composition)

--- zio-v1/zionomicon/src/main/scala/com/sua/chapter02/e06.scala
+++ zio-v1/zionomicon/src/main/scala/com/sua/chapter02/e06.scala
@@ -1,20 +1,16 @@
 package com.sua.chapter02
 
-import zio.{Task, ZIO}
-
-/**
- * Implement the 'zipWith' function in terms of a ZIO effect
- * toy's model. The function should return an effect that sequentially
- * compose specified effects, merging their result with the
- * a user effect defined
- */
+/** Implement the 'zipWith' function in terms of a ZIO effect toy's model. The
+  * function should return an effect that sequentially compose specified
+  * effects, merging their result with the a user effect defined
+  */
 
 object e06ZipWith {
   final case class ToyZIO[-R, +E, +A](run: R => Either[E, A])
 
   def zipWith[R, E, A, B, C](
       self: ToyZIO[R, E, A],
-      that: ToyZIO[R, E, B]
+      that: ToyZIO[R, E, B],
   )(merge: (A, B) => C): ToyZIO[R, E, C] =
     ToyZIO { environment =>
       for {
--- zio-v1/zionomicon/src/main/scala/com/sua/chapter02/e07.scala
+++ zio-v1/zionomicon/src/main/scala/com/sua/chapter02/e07.scala
@@ -1,45 +1,19 @@
 package com.sua.chapter02
 
-import scala.annotation.tailrec
-
 /** Implement the 'collectAll' function in terms of a ZIO effect toy's model.
   * The function should return an effect that sequentially collects the results
   * of the specified collection effects
   */
 
 object e07CollectAll {
-  final case class ToyZIO[-R, +E, +A](run: R => Either[E, A])
+  import e06ZipWith.{zipWith, ToyZIO}
 
-  def collectAll[R, E, A](
-      input: Iterable[ToyZIO[R, E, A]],
-  ): ToyZIO[R, E, List[A]] = {
-
-    @tailrec
-    def collect(
-        input: Iterable[ToyZIO[R, E, A]],
-        accumulator: Either[E, List[A]],
-        environment: R,
-    ): Either[E, List[A]] =
-      if (input.isEmpty) accumulator
-      else {
-        input.head.run(environment) match {
-          case Left(error) => Left(error)
-          case Right(success) =>
-            accumulator match {
-              case left @ Left(_) => left
-              case Right(list) =>
-                collect(input.tail, Right(list :+ success), environment)
-            }
-        }
-
-        ToyZIO { environment =>
-          collect(input, Right(List.empty[A]), environment)
-        }
-      }
-  }
+  def succeed[A](a: => A): ToyZIO[Any, Nothing, A] =
+    ToyZIO(_ => Right(a))
 
   def collectAll[R, E, A](
       input: Iterable[ToyZIO[R, E, A]],
   ): ToyZIO[R, E, List[A]] =
-    ???
+    if (input.isEmpty) succeed(List.empty)
+    else zipWith(input.head, collectAll(input.tail))(_ :: _)
 }
