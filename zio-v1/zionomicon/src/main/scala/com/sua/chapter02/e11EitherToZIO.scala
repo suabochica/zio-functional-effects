a41a0b1dbbef8577e3da635dd71582f5c4e6bfdf
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-09-24 13:57:54 -0500

    zionomicon: from either to ZIO

---
+++ zio-v1/zionomicon/src/main/scala/com/sua/chapter02/e11EitherToZIO.scala
@@ -0,0 +1,14 @@
+package com.sua.chapter02
+
+import zio.ZIO
+
+/** Using `ZIO.fail` and `ZIO.succeed`, implement the following function, which
+  * converts an `Either` into a ZIO effect:
+  */
+
+object e11EitherToZIO {
+
+  def eitherToZIO[E, A](either: Either[E, A]): ZIO[Any, E, A] =
+    either.fold(error => ZIO.fail(error), success => ZIO.succeed(success))
+
+}
