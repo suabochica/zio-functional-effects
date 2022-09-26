88e27ba2ed404dc0846743d5d27fdd4e90f11ca3
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-09-26 09:20:46 -0500

    zionomicon: list to ZIO exercise

---
+++ zio-v1/zionomicon/src/main/scala/com/sua/chapter02/e12ListToZIO.scala
@@ -0,0 +1,16 @@
+package com.sua.chapter02
+
+import zio.ZIO
+
+/** Using `ZIO.fail` and `ZIO.succeed`, implement the following function, which
+  * converts a `List` into a ZIO effect, by looking at the head element in the
+  * list and ignoring the rest of the elements.
+  */
+object e12ListToZIO {
+
+  def listToZIO[A](list: List[A]): ZIO[Any, None.type, A] =
+    list match {
+      case a :: _ => ZIO.succeed(a)
+      case Nil    => ZIO.fail(None)
+    }
+}
