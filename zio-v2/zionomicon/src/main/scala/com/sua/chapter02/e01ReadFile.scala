5fca8f8cc134a2cc427ffe3cb52e57a4e4213315
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-11-23 07:24:48 -0500

    zionomicon: zio v2, solve read file exercise

---
+++ zio-v2/zionomicon/src/main/scala/com/sua/chapter02/e01ReadFile.scala
@@ -0,0 +1,23 @@
+package com.sua.chapter02
+
+import scala.io.Source
+import zio.*
+
+/**
+ * Implement a ZIO version of the function `readFile` by using the
+ * `ZIO.attempt` constructor.
+ */
+
+object e01ReadFile {
+  def readFile(filePath: String): String = {
+    val source = Source.fromFile(filePath)
+
+    try source.getLines.mkString("\n")
+    finally source.close()
+  }
+
+  def readFileZio(filePath: String): ZIO[Any, Throwable, String] = {
+    ZIO.attempt(readFile(filePath))
+  }
+}
+
---
+++ zio-v2/zionomicon/src/main/scala/com/sua/chapter02/e02WriteFile.scala
@@ -0,0 +1,17 @@
+package com.sua.chapter02
+
+import zio.ZIO
+
+/**
+ * Implement a ZIO version of the function `writeFile` by using the
+ * `ZIO.attempt` constructor
+ */
+object e02WriteFile {
+  def writeFile = {
+   ???
+  }
+
+  def writeFileZIO = {
+    ???
+  }
+}
