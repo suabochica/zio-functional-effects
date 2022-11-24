240a76083e31b0dc4ccaf98b367169cd52e9f6f8
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-11-24 08:09:52 -0500

    zionomicon: zio v2, solve write file exercise

--- zio-v2/zionomicon/src/main/scala/com/sua/chapter02/e02WriteFile.scala
+++ zio-v2/zionomicon/src/main/scala/com/sua/chapter02/e02WriteFile.scala
@@ -1,17 +1,22 @@
 package com.sua.chapter02
 
-import zio.ZIO
+import java.io.File
+import java.io.PrintWriter
+import zio.{Task, ZIO}
 
 /**
  * Implement a ZIO version of the function `writeFile` by using the
  * `ZIO.attempt` constructor
  */
 object e02WriteFile {
-  def writeFile = {
-   ???
+  def writeFile(filePath: String, text: String): Unit = {
+    val printWriter = new PrintWriter(new File(filePath))
+
+    try printWriter.write(text)
+    finally printWriter.close()
   }
 
-  def writeFileZIO = {
-    ???
+  def writeFileZIO(filePath: String, text: String): Task[Unit] = {
+    ZIO.attempt(writeFile(filePath, text))
   }
 }
