5b5aac0fc126230532439bf83339cf73d82d11e3
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-11-29 15:09:28 -0500

    zionomicon: zio v2, solve collect all exercise

---
+++ zio-v2/zionomicon/src/main/scala/com/sua/chapter02/e07CollectAll.scala
@@ -0,0 +1,21 @@
+package com.sua.chapter02
+
+import zio.ZIO
+
+/**
+ * Implement the `collectAll` function in terms of the toy model of a ZIO
+ * effect. The function should return an effect that sequentially collects
+ * the results of the specified collection of effects.
+ */
+object e07CollectAll {
+  import e06ZipWith.{zipWith, ToyZIO}
+
+  def succeed[A](a: => A): ZIO[Any, Nothing, A] =
+    ToyZIO(_ => Right(a))
+
+  def collectAll[R, E, A] (
+    input: Iterable[ToyZIO[R, E, A]]
+  ): ToyZIO[R, E, List[A]] =
+    if (input.isEmpty) succeed(List.empty)
+    else zipWith(input.head, collectAll(input.tail))(_ :: _)
+}
