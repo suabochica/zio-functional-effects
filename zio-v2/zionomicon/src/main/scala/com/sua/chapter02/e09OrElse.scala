35d14a8bfa0318cced0edcf07456b7561cba5c59
Sergio L. Benítez D <sergio.kun21@gmail.com>
2022-12-01 08:58:08 -0500

    zionomicon: zio v2, solve or else exercise

---
+++ zio-v2/zionomicon/src/main/scala/com/sua/chapter02/e09OrElse.scala
@@ -0,0 +1,21 @@
+package com.sua.chapter02
+
+/**
+ * Implement the 'orElse' function in terms of the toy model of a ZIO effect.
+ * The function should return an effect that tries the left hand side, but if
+ * that effect fails, it will fallback to the effect on the right hand side.
+ */
+object e09OrElse {
+  import e06ZipWith.ToyZIO
+
+  def orElse[R, E1, E2, A](
+     self: ToyZIO[R, E1, A],
+     that: ToyZIO[R, E2, A]
+  ): ToyZIO[R, E2, A] =
+    ToyZIO { environment =>
+      self.run(environment) match {
+        case Left(e1) => that.run(environment)
+        case Right(a) => Right(a)
+      }
+    }
+}
