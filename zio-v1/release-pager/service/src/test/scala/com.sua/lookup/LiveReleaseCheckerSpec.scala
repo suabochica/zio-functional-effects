70499ffc843087518e55c8295f1870f515cb3e9a
Sergio L. Benítez D <sergio.kun21@gmail.com>
2023-01-11 09:17:25 -0500

    release-pager: add no subscribers and no call services if there are no repositories

--- zio-v1/release-pager/service/src/test/scala/com.sua/lookup/LiveReleaseCheckerSpec.scala
+++ zio-v1/release-pager/service/src/test/scala/com.sua/lookup/LiveReleaseCheckerSpec.scala
@@ -1,14 +1,40 @@
 package com.sua.lookup
 
-import zio.test.{DefaultRunnableSpec, ZSpec, assertCompletes}
+import com.sua.Generators.repositoryName
+import com.sua.client.github.GitHubClient.GitHubClient
+import com.sua.client.github.{GitHubClient, GithubClientMock}
+import com.sua.client.telegram.TelegramClient.TelegramClient
+import com.sua.client.telegram.{ChatId, TelegramClient}
+import com.sua.log.Logger
+import com.sua.subscription.Repository.{Name, Version}
+import com.sua.subscription.SubscriptionLogic.SubscriptionLogic
+import com.sua.subscription.SubscriptionLogicMock
+import zio.ULayer
+import zio.test.Assertion.equalTo
+import zio.test.mock.Expectation.unit
+import zio.test.{DefaultRunnableSpec, ZSpec, assertCompletes, checkM}
 
 object LiveReleaseCheckerSpec extends DefaultRunnableSpec {
   override def spec: ZSpec[Environment, Failure] = suite("Live ReleaseCheckerSpec")(
     testM("Do not call services if there are no repositories") {
-      ???
+      val gitHubClientMocks: ULayer[GitHubClient] = GitHubClient.empty
+      val telegramClientMocks: ULayer[TelegramClient] = TelegramClient.empty
+      val subscriptionLogicMocks: ULayer[SubscriptionLogic] = SubscriptionLogicMock.ListRepositories(value(Map.empty[Name, Option[Version]])) ++
+        SubscriptionLogicMock.UpdateVersions(equalTo(Map.empty[Name, Version]), unit)
+
+      refreshSchedule(gitHubClientMocks, telegramClientMocks, subscriptionLogicMocks)
     },
     testM("Do not bother subscribers if there are no version updates") {
-      ???
+      checkM(repositoryName) { name =>
+        val repositories = Map(name -> Some(finalVersion))
+        val githubClientMocks: ULayer[GitHubClient] = GithubClientMock.Releases(equalTo(name), value(List(finalRelease)))
+        val telegramClientMocks: ULayer[TelegramClient] = TelegramClient.empty
+        val subscriptionLogicMocks = ULayer[SubscriptionLogic] =
+          SubscriptionLogicMock.ListRepositories(value(repositories)) ++
+            SubscriptionLogicMock.UpdateVersions(equalTo(Map(empty[Name, Version]), unit))
+
+        refreshSchedule(githubClientMocks, telegramClientMocks, subscriptionLogicMocks)
+      }
     },
     testM("Update repository version for the very first time") {
       ???
@@ -28,7 +54,7 @@ object LiveReleaseCheckerSpec extends DefaultRunnableSpec {
                              ): ZIO[ZEnv, Throwable, TestResult] = {
     val layer = (Logger.silent ++ gitHubClient ++ telegramClient ++ subscriptionLogic) >>> ReleaseChecker.live
 
-    releaseChecker
+    ReleaseChecker
       .refreshSchedule
       .provideLayer(layer)
       .as(assertCompletes)
